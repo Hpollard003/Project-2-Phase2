cf286725204334403011b85d74fb2de4b00d40c4
Hpollard003 <hpollard003@gmail.com>
2021-07-03 03:20:19 -0400

    done. still needs some styling

--- src/components/PostGallery.js
+++ src/components/PostGallery.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import dog from '../imgs/dog.jpg'
+import post1 from '../imgs/Post2.jpg'
+import post2 from '../imgs/cat.jpg'
 
 function PostGallery() {
     return (
         <div>
-            <img />
+            <img src={dog} alt='dog' />
+            <img src={post1} alt='anime' />
+            <img src={post2} alt='cat' />
         </div>
     )
 }
