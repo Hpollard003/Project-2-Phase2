ee5ecde25daa975e8d5b26475001d471e259383f
Hpollard003 <hpollard003@gmail.com>
2021-07-15 13:03:26 -0400

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,17 +4,17 @@ import { NavLink } from 'react-router-dom';
 function NavBar() {
     return (
         <div className="nav justify-content-center">
-              <ul class="nav nav-tabs">
-      <li class="nav-item">
+              <ul className="nav nav-tabs">
+      <li className="nav-item">
       <NavLink className="nav-link" to="/">Home</NavLink>
       </li>
-      <li class="nav-item">
+      <li className="nav-item">
       <NavLink className="nav-link active" to="/about"> About Page</NavLink>
       </li>
-      <li class="nav-item">
+      <li className="nav-item">
       <NavLink className="nav-link active" to="/posts"> New Posts</NavLink>
       </li>
-      <li class="nav-item">
+      <li className="nav-item">
       <NavLink className="nav-link active" to="/oldposts"> Old Posts</NavLink>
       </li>
     </ul>
